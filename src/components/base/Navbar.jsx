46e48b8075a494a92c1f58c53817907be823ed23
Jdentity <identityjang@gmail.com>
2022-12-25 01:56:47 +0900

    🚚 Add components folder
    컴포넌트 폴더에 컴포넌트 이동

--- src/components/base/Navbar.jsx
+++ src/components/base/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import Logo from '../assets/logo_mini.svg';
+import Logo from '../../assets/logo_mini.svg';
 
 const Navbar = () => {
   const navigate = useNavigate();
