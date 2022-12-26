2beb02f855710d4bc674121a3034e70f7b0cf4ae
Jdentity <identityjang@gmail.com>
2022-12-26 17:30:41 +0900

    💄 Divide UserBadge component
    UserBadge 컴포넌트 분리

--- src/components/community/MyProfile.jsx
+++ src/components/community/MyProfile.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import theme from '../../styles/theme';
 import { useNavigate } from 'react-router-dom';
 import UserAvatar from '../user/UserAvatar';
+import UserBadge from '../user/UserBadge';
 
 const MyProfile = () => {
   const navigate = useNavigate();
@@ -129,36 +130,6 @@ const UserBadgeBox = styled.div`
     &:not(:last-child) {
       margin-right: 4px;
     }
-  }
-`;
-
-const UserBadge = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0 4px;
-  height: 19px;
-  background: #f6f6f6;
-  border-radius: 4px;
-  font-weight: 700;
-  font-size: 10px;
-  line-height: 19px;
-  color: #888;
-
-  // 0 웹개발 1 서버 2 프론트 3 QA 4 DevOps
-  &[data-id='0'] {
-    color: #8958fa;
-  }
-  &[data-id='1'] {
-    color: #5866dc;
-  }
-  &[data-id='2'] {
-    color: #008ae2;
-  }
-  &[data-id='3'] {
-    color: #f07c08;
-  }
-  &[data-id='4'] {
-    color: #8958fa;
   }
 `;
 
