0c3f5860b3bf3799277bdcc0e8b3bc2155d2a8cd
123456soobin-choi <sholong56@gmail.com>
2022-12-28 15:59:59 +0900

    ♻️ Refactor SharePost component - div 컴포넌트들을 styled 컴포넌트로 변경하여 각각 스타일 적용함

--- src/components/post/postDetail/SharePost.jsx
+++ src/components/post/postDetail/SharePost.jsx
@@ -3,37 +3,37 @@ import styled from 'styled-components';
 import share from '../../../assets/share.png';
 
 const SharePost = () => {
-  const handleCopy = () => {
+  const handleCopyUrl = () => {
     navigator.clipboard.writeText(window.location.href);
     alert('링크가 복사되었습니다'); // 테스트
   };
 
   return (
     <StSharePost>
-      <button type="button" onClick={handleCopy}>
+      <ShareBtn type="button" onClick={handleCopyUrl}>
         <img src={share} alt="share icon" />
-      </button>
+      </ShareBtn>
     </StSharePost>
   );
 };
 
 const StSharePost = styled.div`
   margin-right: 20px;
+`;
 
-  button {
-    position: absolute;
-    right: 46px;
-    bottom: -3px;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    background: none;
-    cursor: pointer;
+const ShareBtn = styled.div`
+  position: absolute;
+  right: 46px;
+  bottom: -3px;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: none;
+  cursor: pointer;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  img {
+    width: 20px;
+    height: 20px;
   }
 `;
 
