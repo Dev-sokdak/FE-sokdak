e895eca9093d7da4b06c3a8de3fbe178721dbf6b
123456soobin-choi <sholong56@gmail.com>
2022-12-28 16:06:53 +0900

    💄 Add DotComment component 댓글 수정 버튼 컴포넌트 추가

---
+++ src/components/post/comment/DotComment.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import DotCommentPopUp from './DotCommentPopUp';
+import DeleteCommentModal from './DeleteCommentModal';
+import more from '../../../assets/more.png';
+
+const DotComment = () => {
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleShowPopUp = () => {
+    setShowPopUp(true);
+  };
+
+  return (
+    <StDotComment>
+      {showPopUp && (
+        <DotCommentPopUp
+          setShowPopUp={setShowPopUp}
+          setModalOpen={setModalOpen}
+        />
+      )}
+      {modalOpen && <DeleteCommentModal setModalOpen={setModalOpen} />}
+      <DotCommentBtn type="button" onClick={handleShowPopUp}>
+        <img src={more} alt="more icon" />
+      </DotCommentBtn>
+    </StDotComment>
+  );
+};
+
+const StDotComment = styled.div`
+  margin-left: auto;
+  position: relative;
+  display: flex;
+`;
+
+const DotCommentBtn = styled.div`
+  height: 18px;
+  color: #767676;
+  cursor: pointer;
+
+  img {
+    width: 18px;
+    height: 18px;
+  }
+`;
+
+export default DotComment;
