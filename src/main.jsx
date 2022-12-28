a4ef5330c5a0c9ae64ac50885ca678f58a7eed6e
Jdentity <identityjang@gmail.com>
2022-12-28 17:59:19 +0900

    👔 Add Cookie Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
+import { CookiesProvider } from 'react-cookie';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
+    <CookiesProvider>
+      <GlobalStyle />
+      <App />
+    </CookiesProvider>
   </ThemeProvider>,
 );
